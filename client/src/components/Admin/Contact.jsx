3cba97cf5d33cc12a12796996330a7e3a8f211ca
Yawi <yawimalik786@gmail.com>
2025-05-21 16:00:55 +0530

    Added toggle feature

--- client/src/components/Admin/Contact.jsx
+++ client/src/components/Admin/Contact.jsx
@@ -1,33 +1,34 @@
 import React, { useEffect, useState } from "react";
-import {getContacts} from '../../Redux/contactSlice'
-import {useDispatch,useSelector} from 'react-redux'
+import { getContacts } from "../../Redux/contactSlice";
+import { useDispatch, useSelector } from "react-redux";
 import { updateContacts } from "../../Redux/contactSlice";
-import {toast} from 'react-toastify'
+import { toast } from "react-toastify";
 
 const ContactTable = () => {
-  const {contacts} = useSelector(state=>state.contact) ;
-  const dispatch = useDispatch()
+  const { contacts } = useSelector((state) => state.contact);
+  const dispatch = useDispatch();
 
-  useEffect(()=>{
-    dispatch(getContacts())
-  },[dispatch])
+  useEffect(() => {
+    dispatch(getContacts());
+  }, [dispatch]);
 
-  const handleStatusUpdate = async(id,status)=>{
-       const newStatus = status==='pending'? 'resolved':'pending';
-      
+  const handleStatusUpdate = async (id, status) => {
+    const newStatus = status === "pending" ? "resolved" : "pending";
+
     try {
       await dispatch(updateContacts({ id, status: { status: newStatus } }));
-      toast('Status updated successfully')
+      toast("Status updated successfully");
     } catch (error) {
-      console.log('Error at status change :',error)
+      console.log("Error at status change :", error);
     }
-  }
+  };
 
   return (
     <div className="p-3  min-h-screen">
-
       <div className="max-w-6xl mx-auto bg-white rounded-xl shadow-lg p-6">
-        <h2 className="text-2xl md:text-3xl font-bold mb-4 text-gray-800">🤙 Contacts </h2>
+        <h2 className="text-2xl md:text-3xl font-bold mb-4 text-gray-800">
+          🤙 Contacts{" "}
+        </h2>
         <div className="overflow-x-auto rounded-lg">
           <table className="min-w-full divide-y divide-gray-200">
             <thead className="bg-gray-50 text-gray-600 text-left text-sm font-semibold">
@@ -52,7 +53,7 @@ const ContactTable = () => {
                   <td className="p-4">{contact.message}</td>
                   <td className="p-4">
                     <span
-                      className={`px-3 py-1 rounded-full text-xs font-medium ${
+                      className={`px-3 py-1 rounded-full text-xs font-medium capitalize ${
                         contact.status === "resolved"
                           ? "bg-green-100 text-green-700"
                           : "bg-yellow-100 text-yellow-700"
@@ -62,14 +63,29 @@ const ContactTable = () => {
                     </span>
                   </td>
                   <td className="p-4">
-                    <button
-                      onClick={() =>
-                        handleStatusUpdate(contact._id, contact.status)
-                      }
-                      className="text-white bg-blue-600 hover:bg-blue-700 px-4 py-1 rounded-full text-sm"
+                    <label
+                      className={`w-12 h-6 inline-block ${
+                        contact.status === "resolved"
+                          ? "bg-green-600"
+                          : "bg-gray-400"
+                      } p-2 rounded-4xl relative cursor-pointer`}
+                      htmlFor={`toggle-${contact._id}`}
                     >
-                      Toggle Status
-                    </button>
+                      <input
+                        type="checkbox"
+                        className="sr-only"
+                        id={`toggle-${contact._id}`}
+                        checked={contact.status === "resolved"}
+                        onChange={() =>
+                          handleStatusUpdate(contact._id, contact.status)
+                        }
+                      />
+                      <span
+                        className={`absolute left-0 top-0 h-6 w-6 rounded-full  bg-white/90 transition-transform duration-200 ${
+                          contact.status === "resolved" ? "translate-x-6" : ""
+                        }`}
+                      ></span>
+                    </label>
                   </td>
                 </tr>
               ))}
