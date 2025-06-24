3ec20f608da73db181c363acd16cfaf1c3b7616f
Yawi <yawimalik786@gmail.com>
2025-06-24 22:53:58 +0530

    Changed the CORS

--- server/server.js
+++ server/server.js
@@ -17,19 +17,19 @@ const contactRouter = require('./src/routes/contact.routes.js')
 const app = express();
 
 // CORS configuration
-let corsOptions = {
-  origin: [
-    "http://localhost:5173",
-    "https://nutricare11.netlify.app",
-  ],
-  methods: "GET,POST,PUT,DELETE",
-  credentials: true,
-};
+// let corsOptions = {
+//   origin: [
+//     "http://localhost:5173",
+//     "https://nutricare11.netlify.app",
+//   ],
+//   methods: "GET,POST,PUT,DELETE",
+//   credentials: true,
+// };
 
 // Middlewares
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(cookieParser());
 
 // API Routes
